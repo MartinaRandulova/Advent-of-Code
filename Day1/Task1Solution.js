123421cb87699250ed5ffe1d91dd23e93fc6781d
rubesovm <rubesovm@gmail.com>
2022-12-01 21:32:11 +0100

    refactor of 1st day

--- Day1/Task1Solution.js
+++ Day1/Task1Solution.js
@@ -3,17 +3,16 @@ const { readFileSync } = require('fs');
 const contents = readFileSync('./input.txt', 'utf-8');
 
 let allSums = []
-let sum = 0;
 
-Array.from(contents.split('\n')).forEach((row) => {
-  if (row != '') {
-    sum += Number(row)
-  } else {
-    allSums.push(sum);
-    sum = 0;
+contents.split('\n').reduce((sum, calVal) => {
+  if (calVal != '') {
+    return sum + Number(calVal)
   }
-});
+  allSums.push(sum);
+  return 0;
+}, 0);
 
-allSums.sort((a,b) => a < b ? 1 : -1);
-console.log(allSums[0]) //Task 1
+allSums.sort((a, b) => a < b ? 1 : -1);
+console.log(allSums[0])                           //Task 1
 console.log(allSums[0] + allSums[1] + allSums[2]) //Task 2
+
