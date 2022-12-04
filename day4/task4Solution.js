cda25afcc36ebd0c3cb30cc288bbe26b4e2c8684
rubesovm <rubesovm@gmail.com>
2022-12-04 21:05:49 +0100

    variables unification in day 4 solution

--- day4/task4Solution.js
+++ day4/task4Solution.js
@@ -3,7 +3,7 @@ const { readFileSync } = require('fs');
 const contentsArr = readFileSync('./input.txt', 'utf-8').split('\n');
 
 let fullyContainsCount = 0;
-let onlyOverlaps = 0;
+let onlyOverlapsCount = 0;
 
 contentsArr.forEach(row => {
   let boardersNum = row.split(/,|-/).map(boarder => Number(boarder));
@@ -12,10 +12,10 @@ contentsArr.forEach(row => {
   if ((boardersNum[0] <= boardersNum[2] && boardersNum[1] >= boardersNum[3]) || (boardersNum[0] >= boardersNum[2] && boardersNum[1] <= boardersNum[3])) { fullyContainsCount++; }
 
   // Task 2
-  if ((boardersNum[1] <= boardersNum[2] && boardersNum[0] >= boardersNum[3]) || (boardersNum[1] >= boardersNum[2] && boardersNum[0] <= boardersNum[3])) { onlyOverlaps++; }
+  if ((boardersNum[1] <= boardersNum[2] && boardersNum[0] >= boardersNum[3]) || (boardersNum[1] >= boardersNum[2] && boardersNum[0] <= boardersNum[3])) { onlyOverlapsCount++; }
 
 });
 console.log(fullyContainsCount);
-console.log(onlyOverlaps);
+console.log(onlyOverlapsCount);
 
 
